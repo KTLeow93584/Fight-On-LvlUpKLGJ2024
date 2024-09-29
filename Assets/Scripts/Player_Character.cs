589db185555183771326473648307b5f4021c9fd
Leow Kean Tat (Kaz) <leowkeantat@gmail.com>
2024-09-29 13:56:18 +0800

    Fixed incorrect import of a unity editor component.

--- Assets/Scripts/Player_Character.cs
+++ Assets/Scripts/Player_Character.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player_Character : MonoBehaviour
 {
